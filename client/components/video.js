baf3ce32f4a6ebb2fb7cae9c028e7930ee814a9e
kuperavv <vince.kuperavage@gmail.com>
2021-03-06 11:23:49 -0500

    vids

--- client/components/video.js
+++ client/components/video.js
@@ -28,7 +28,7 @@ const constraints = {
 class Video extends Component {
   constructor(props) {
     super(props);
-    this.state = { members: [], videos: [] };
+    this.state = { members: [], videos: [], id: [] };
   }
   componentDidMount() {
     const video = document.getElementById('self');
@@ -51,11 +51,11 @@ class Video extends Component {
         .getTracks()
         .forEach((track) => peerConnection.addTrack(track, stream));
 
-      const other = document.getElementById('other');
-      peerConnection.ontrack = (event) => {
-        other.srcObject = event.streams[0];
-      };
-
+      //const other = document.getElementById('other');
+      //peerConnection.ontrack = (event) => {
+      //  other.srcObject = event.streams[0];
+      //};
+      //console.log(other);
       peerConnection.onicecandidate = (event) => {
         if (event.candidate) {
           socket.emit('candidate', id, event.candidate);
@@ -75,7 +75,11 @@ class Video extends Component {
       //     [id]: peerConnection,
       //   },
       // });
-      this.setState({ members: [...this.state.members, id] });
+      this.setState({
+        members: [...this.state.members, peerConnection],
+        id: [...this.state.id, id],
+      });
+      // this.setState({ id: [...this.state.id, id] });
     });
 
     socket.on('answer', (id, description) => {
@@ -97,7 +101,12 @@ class Video extends Component {
       //NEW
       // const peerConnection = new RTCPeerConnection(config);
       peerConnections[id] = peerConnection;
-      this.setState({ members: [...this.state.members, id] });
+
+      const video = document.getElementById('self');
+      let stream = video.srcObject;
+      stream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, stream));
 
       peerConnection
         .setRemoteDescription(description)
@@ -115,10 +124,10 @@ class Video extends Component {
       // userVideo.muted = true;
       //const videos = document.getElementById('videos');
       //videos.appendChild(userVideo);
-      const other = document.getElementById('other');
-      peerConnection.ontrack = (event) => {
-        other.srcObject = event.streams[0];
-      };
+      //const other = document.getElementById('other');
+      //peerConnection.ontrack = (event) => {
+      //  other.srcObject = event.streams[0];
+      //};
 
       //this.setState({ videos: [...this.state.videos, userVideo] });
 
@@ -127,6 +136,11 @@ class Video extends Component {
           socket.emit('candidate', id, event.candidate);
         }
       };
+      this.setState({
+        members: [...this.state.members, peerConnection],
+        id: [...this.state.id, id],
+      });
+      // this.setState({ id: [...this.state.id, id] });
     });
 
     socket.on('candidate', (id, candidate) => {
@@ -145,8 +159,35 @@ class Video extends Component {
     });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prev', prevProps, prevState);
+    //console.log(this);
+
+    if (
+      !prevState.members ||
+      prevState.members.length !== this.state.members.length
+    ) {
+      if (this.state.members.length > 0) {
+        let newVideo;
+        if (!prevState.id) {
+          newVideo = [this.state.id[0]];
+        } else {
+          console.log('state', this.state);
+          newVideo = this.state.id.filter((id) => !prevState.id.includes(id));
+        }
+
+        console.log(newVideo);
+        const idx = this.state.id.indexOf(newVideo[0]);
+        console.log(idx);
+        this.state.members[idx].ontrack = (event) => {
+          this[newVideo[0]].srcObject = event.streams[0];
+        };
+      }
+    }
+  }
+
   render() {
-    console.log(this.state);
+    //console.log(this.state);
     return (
       <div id="videos">
         <video
@@ -156,13 +197,56 @@ class Video extends Component {
           autoPlay
           muted
         ></video>
-        <video
+
+        {/* {this.state.members.map((el, idx) => {
+          return (
+            <video
+              id={Object.keys(peerConnections)[idx]}
+              className="video_player"
+              playsInline
+              autoPlay
+              muted
+              key={idx}
+              ref={(videooo) => {
+                this.videooo = videooo;
+              }}
+              // srcObject={
+              //   (el.ontrack = (event) => {
+              //     return event.streams[0];
+              //   })
+              //}
+            ></video>
+          );
+        })} */}
+
+        {Object.keys(peerConnections).map((peer, idx) => {
+          return (
+            <video
+              id={peer}
+              className="video_player"
+              playsInline
+              autoPlay
+              muted
+              key={idx}
+              ref={(vid) => {
+                this[peer] = vid;
+              }}
+              // srcObject={
+              //   (el.ontrack = (event) => {
+              //     return event.streams[0];
+              //   })
+              //}
+            ></video>
+          );
+        })}
+
+        {/* <video
           id="other"
           className="video_player"
           playsInline
           autoPlay
           muted
-        ></video>
+        ></video> */}
       </div>
     );
   }
